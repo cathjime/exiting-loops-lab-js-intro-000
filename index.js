182b3050781e299be87c12b5bf28b1c974a52a4f
Catherine Jimenez <xcatherine.jimenez@gmail.com>
2020-04-02 21:26:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,3 +7,11 @@ function breakOut(array, changeValue, stopValue) {
   }
   return array;
 }
+
+function keepGoing(array, changeValue, skipValue){
+  for(var i = 0; i < array.length; i++){
+    array[i] = changeValue;
+    if(array[i] === skipValue){
+      continue;
+    }
+}
