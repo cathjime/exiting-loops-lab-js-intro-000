73f0f245476bea68de6f6fc8d2c47a96dd7c81df
Catherine Jimenez <xcatherine.jimenez@gmail.com>
2020-04-02 21:25:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 function breakOut(array, changeValue, stopValue) {
-  
+  for(var i = 0; i < array.length; i++){
+    array[i] = changeValue;
+    if(array[i] === stopValue){
+      break;
+    }
+  }
 }
